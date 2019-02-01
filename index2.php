1c1617ac8fe6696f8d160e205fa9174564464d84
Roman <rblondel23@gmail.com>
2019-02-01 14:09:18 +0100

    added index2.php

--- index2.php
+++ index2.php
@@ -1,68 +1,44 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Recaptcha Demo</title>
-    <script src='https://www.google.com/recaptcha/api.js'></script>
-</head>
-<body>
-
 <?php
-// Checks if form has been submitted
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    function post_captcha($user_response) {
-        $fields_string = '';
-        $fields = array(
-            'secret' => '6Lc77o0UAAAAAJ6fD8aUeEIgAbSFtArLr1QyPpEZ',
-            'response' => $user_response
-        );
-        foreach($fields as $key=>$value)
-            $fields_string .= $key . '=' . $value . '&';
-        $fields_string = rtrim($fields_string, '&');
+if(isset($_POST['name'])){
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
-        curl_setopt($ch, CURLOPT_POST, count($fields));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+    $name=$_POST['name'];
+    $g_resp=$_POST['g-recaptcha-response'];
 
-        $result = curl_exec($ch);
-        curl_close($ch);
+    $secret="your-secret-key"; // Secret key provided by google
 
-        return json_decode($result, true);
+    $postdata="secret=".$secret."&response=".$g_resp; // we will post the data to the google
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $server_output = curl_exec($ch); // response from the google
+    $out=json_decode($server_output);
+    curl_close ($ch);
+
+    if($out->success ==1){
+        // after success
+        // place your code here what you want to do.
+        echo "capcha sucess <br>";
+        echo $name;
+
+    }else{
+        // failure
+        // place the fail code.
+        echo "captcha failure try again";
     }
+}
 
-    // Call the function post_captcha
-    $res = post_captcha($_POST['g-recaptcha-response']);
 
-    if (!$res['success']) {
-        // What happens when the CAPTCHA wasn't checked
-        echo '<p>Please go back and make sure you check the security CAPTCHA box.</p><br>';
-    } else {
-        // If CAPTCHA is successfully completed...
+?>
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<form method="post" >
 
-        // Paste mail function or whatever else you want to happen here!
-        echo '<br><p>CAPTCHA was completed successfully!</p><br>';
-    }
-} else { ?>
+    name <input type="" name="name" />
+    <!-- insert the sitekey provided by google -->
+    <div class="g-recaptcha" data-sitekey="your-site-key"></div>
+    <input type="submit" >
 
-    <!-- FORM GOES HERE -->
-    <form action="index.php" method="post">
-        <label for="fname">First Name*</label><br>
-        <input type="text" name="fname" id="fname" required autofocus><br><br>
-
-        <label for="lname">Last Name*</label><br>
-        <input type="text" name="lname" id="lname" required><br><br>
-
-        <label for="email">Email Address*</label><br>
-        <input type="email" name="email" id="email" required><br><br>
-
-        <div class="g-recaptcha" data-sitekey="6Lc77o0UAAAAAJ7OGhtjelXc7EVT7i_jaEPzAR-s"></div>
-        <br>
-        <input type="submit" id="submit" value="Submit">
-    </form>
-
-<?php } ?>
-
-</body>
-</html>
+</form>
